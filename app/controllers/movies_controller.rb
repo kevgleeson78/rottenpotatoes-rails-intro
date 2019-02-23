894153f40cf362978c82eab57c8e2dacc1907eb2
Your NameKevin <kevgleeson78@gmail.com>
2019-02-23 05:26:12 +0000

    test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -39,9 +39,9 @@ class MoviesController < ApplicationController
         @ratings = @all_ratings
       end
   end
-  @selected_ratings = (params[:ratings].present? ? params[:ratings] : [])
+ 
   @movies = Movie.order(session[:order]).where(:rating => @ratings)
-
+  @selected_ratings = (params[:ratings].present? ? params[:ratings] : [])
    
     
     
